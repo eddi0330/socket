2e193dc954708917a16090bdcad655800cf5fe35
eddi0330 <myrdariedisson@gmail.com>
2022-02-15 10:57:55 +0100

    commit a caso

--- contatore_client.py
+++ contatore_client.py
@@ -4,7 +4,7 @@ import string
 from tokenize import String
 
 HOST="127.0.0.1"
-PORT=65432
+PORT=22016
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
