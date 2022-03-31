1d971909afd52ecd1311a47786c324a30933fd9d
eddi0330 <myrdariedisson@gmail.com>
2022-03-31 12:36:03 +0200

    commit(esbefwhifewihuewfihefwihefvwihjdfsihjeu9eg)

--- pagellaServerMultiMyrdari.py
+++ pagellaServerMultiMyrdari.py
@@ -3,6 +3,7 @@
 import socket
 from threading import Thread
 import json
+import pprint
 
 
 SERVER_ADDRESS = '127.0.0.1'
@@ -77,7 +78,32 @@ def ricevi_comandi2(sock_service,addr_client):
 
 #Versione 3
 def ricevi_comandi3(sock_service,addr_client):
-    pass
+    while True:
+        data=sock_service.recv(1024)
+        if not data: 
+            break
+        data=data.decode()
+        data=json.loads(data)
+        tabellone=[]
+        pp=pprint.PrettyPrinter(indent=4)
+        for studente in data:
+            pagella=data[studente]  
+            assenze=0
+            media=0
+            for i, p in enumerate(pagella):    
+                media+=int(p[1])   
+                assenze+=int(p[2])
+            media=media/i
+            messaggio={'studente':studente,
+            'media':media,
+            'assenze':assenze}  
+            tabellone.append(messaggio)
+        print("Dati inviati: ")
+        pp.pprint(tabellone)
+        messaggio=tabellone
+        messaggio=json.dumps(messaggio)
+        sock_service.sendall(messaggio.encode("UTF-8"))
+    sock_service.close()
   #....
   #1.recuperare dal json il tabellone
   #2. restituire per ogni studente la media dei voti e somma delle assenze :
@@ -89,7 +115,7 @@ def ricevi_connessioni(sock_listen):
         print("\nConnessione ricevuta da " + str(addr_client))
         print("\nCreo un thread per servire le richieste ")
         try:
-            Thread(target=ricevi_comandi2,args=(sock_service,addr_client)).start()
+            Thread(target=ricevi_comandi3,args=(sock_service,addr_client)).start()
         except:
             print("il thread non si avvia")
             sock_listen.close()
